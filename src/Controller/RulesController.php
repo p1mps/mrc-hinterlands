92ff3ac395c9a32ab2105ce2ad5dc24dcecb27b5
Andrea Imparato <a.imparato@rebuy.com>
2026-06-26 22:41:46 +0200

    add rules

---
+++ src/Controller/RulesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RulesController extends AbstractController {
+    #[Route('/rules', name: 'app_rules')]
+    public function index(): Response {
+        return $this->render('rules/index.html.twig');
+    }
+}
