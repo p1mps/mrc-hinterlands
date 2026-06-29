432f69a42a3c5debd2a37e75e2fc8ac43aa49519
Andrea Imparato <a.imparato@rebuy.com>
2026-06-29 09:48:30 +0200

    objectives

--- src/Form/PostTrackFormType.php
+++ src/Form/PostTrackFormType.php
@@ -11,7 +11,21 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class PostTrackFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('combatPayTier', EnumType::class, ['class' => CombatPayTier::class, 'label' => 'Combat Pay Tier'])
+            ->add(
+                'combatPayTier',
+                EnumType::class,
+                ['class' => CombatPayTier::class, 'label' => 'Combat Pay Tier',
+                    'choice_label' => function (CombatPayTier $choice): string {
+                        return match ($choice) {
+                            // Replace these with your actual Enum cases
+                            CombatPayTier::None => 'No objs',
+                            CombatPayTier::Half => 'Less Objs than opponent',
+                            CombatPayTier::Full => 'More Objs than opponent',
+                            CombatPayTier::HalfAgain => 'All Objs',
+                        };
+                    },
+                ],
+            )
             ->add('salvageClaimed', CheckboxType::class, ['required' => false, 'label' => 'Salvage Claimed?']);
     }
 
